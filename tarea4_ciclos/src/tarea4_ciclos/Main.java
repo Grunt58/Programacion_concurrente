065cbcf596e179bed45759749d9ef9754cd6f11b
Grunt58 <freddy.fazbear.troll@gmail.com>
2021-10-05 09:02:36 -0500

    Tarea 4 - Ciclos

---
+++ tarea4_ciclos/src/tarea4_ciclos/Main.java
@@ -0,0 +1,18 @@
+package tarea4_ciclos;
+
+/*
+ *
+ * @author null
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here     
+        SeriesWhile serie = new SeriesWhile();
+
+        serie.CapturarN();
+        serie.Serie3En3();
+        serie.SerieMenosN();
+        serie.Salir();
+    }
+}
