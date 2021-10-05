c05112745d902034ee0a6545b73b83f7f244864b
Grunt58 <freddy.fazbear.troll@gmail.com>
2021-10-05 13:55:04 -0500

    ISCI_actividad_1

---
+++ ISCI_hola_mundo/src/isci_hola_mundo/Main.java
@@ -0,0 +1,28 @@
+package isci_hola_mundo;
+
+/**
+ *
+ * @author Jorge Tirado Uriza
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // Actividad 1
+        System.out.println("Hola mundo");
+        
+        // var
+        var cadena = "Es una cadena";
+        System.out.println(cadena);
+        
+        // No me acuerdo que era XD
+        int a = 10;
+        float b = 4.5f;
+        System.out.println("Los números son " + a + " y " + b);
+        
+        // Concatenación de cadenas
+        var universidad = "Universidad del Valle de México";
+        var campus = "Coyoacán";
+        System.out.println("Universidad: " + universidad + "\nCampus: " + campus);
+    }
+}
