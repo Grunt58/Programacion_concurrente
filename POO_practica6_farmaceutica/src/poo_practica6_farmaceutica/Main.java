bba3a94a8e97f149a2c1b0f3a001c89ad8ef2a86
Grunt58 <freddy.fazbear.troll@gmail.com>
2021-10-07 12:59:04 -0500

    Todas las actividades de la sesión 14 (Hay errores proque se está usando el mismo nombre de variables, comenta esa parte del código)

---
+++ POO_practica6_farmaceutica/src/poo_practica6_farmaceutica/Main.java
@@ -0,0 +1,62 @@
+package poo_practica6_farmaceutica;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Jorge Tirado Uriza
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ListaUsuarios lu = new ListaUsuarios();
+        
+        int op;
+        Scanner input = new Scanner(System.in);
+        do{
+            System.out.println("-----------------FARMACÉUTICA----------------");
+            System.out.println("\t1. Registrar nuevo usuario");
+            System.out.println("\t2. Buscar usuario");
+            System.out.println("\t3. Actualizar usuario");
+            System.out.println("\t4. Borrar usuario");
+            System.out.println("\t5. Ordenar lista de usuarios");
+            System.out.println("\t6. Mostrar lista de usuarios");
+            System.out.println("\t7. Salir del programa.");
+            System.out.println("-----------------FARMACÉUTICA----------------\n\n");
+            System.out.print("Elija una opción: ");
+        
+            op = input.nextInt();
+            System.out.print("\n\n");
+            switch(op){
+                case 1:
+                    lu.Agregar();
+                    break;
+                case 2:
+                    lu.Buscar();
+                    break;
+                case 3:
+                    lu.Actualizar();
+                    break;
+                case 4:
+                    lu.Borrar();
+                    break;
+                case 5:
+                    lu.OrdenarLista();
+                    break;
+                case 6:
+                    lu.MostrarLista();
+                    break;
+                case 7:
+                    break;
+                default:
+                    System.out.println("---------------------------------");
+                    System.out.println("[ERROR]: Fuera de rango.");
+                    System.out.println("---------------------------------");
+                    break;
+            }
+            // Limpiamos el buffer
+            input.nextLine();
+        } while(op != 7);
+        lu.Salir();   
+    }
+}
---
+++ POO_practica6_farmaceutica/src/poo_practica6_farmaceutica/Usuario.java
@@ -0,0 +1,50 @@
+package poo_practica6_farmaceutica;
+import java.util.Scanner;
+/**
+ *
+ * @author Jorge Tirado Uriza
+ */
+public class Usuario {
+
+    // ATRIBUTOS
+    private String nombre;
+    private int noCuenta, edad;
+    private static int idContador = 1;
+    Scanner input = new Scanner(System.in);
+    
+    // CONSTRUCTOR
+    Usuario (String nombre, int edad){
+        System.out.println("---------------------------------");
+        System.out.println("[SERVER]: Nuevo usuario registrado");
+        System.out.println("---------------------------------\n");
+        this.nombre = nombre;
+        this.edad = edad;
+        this.noCuenta = idContador++;   
+    }
+    
+    // GETTERS Y SETTERS
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNoCuenta() {
+        return noCuenta;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    
+    // MÉTODOS  
+    public String MostrarUsuario() {
+        return "Nombre: " + nombre + "|| edad: " + edad + "|| [No. Cuenta: " + noCuenta + "]";   
+    }
+}
